3bad0d411233b310d18c4c25d13e9024e090d930
ernest awuah <ernestawuah@yahoo.com>
2021-05-08 18:23:15 -0400

    added logic to repaly the game

--- src/HotCold.java
+++ src/HotCold.java
@@ -1,3 +1,4 @@
+import javax.print.DocFlavor;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,22 +10,30 @@ import java.util.Scanner;
 
 public class HotCold {
     public static void main(String[] args) {
+        String yesOrNo;
         //generate random number//
-        int rand = new Random().nextInt(1001);
-        Scanner in = new Scanner(System.in);
-        int guess;
         do {
+            int rand = new Random().nextInt(1001);
+            System.out.println(rand);
+            Scanner in = new Scanner(System.in);
+            int guess;
+            do {
 
-            //Tell user to guess a number
-            System.out.println("Guess a number from 0 to a 1000");
-            guess = in.nextInt();
+                //Tell user to guess a number
+                System.out.println("Guess a number from 0 to a 1000");
+                guess = in.nextInt();
 
-            //Display whether guess is hotter or colder
-            //TODO HOTCOLDLOGIC
-            System.out.println("Your Guess Is Cold");
-        } while(guess != rand);
+                //Display whether guess is hotter or colder
+                //TODO HOTCOLDLOGIC
+                System.out.println("Your Guess Is Cold");
+            } while (guess != rand);
+            System.out.println("Do you want play again. YES or NO");
+            in.nextLine();
+            yesOrNo = in.nextLine();
+        } while(yesOrNo.equalsIgnoreCase("YES"));
 
 
 
     }
+
 }
